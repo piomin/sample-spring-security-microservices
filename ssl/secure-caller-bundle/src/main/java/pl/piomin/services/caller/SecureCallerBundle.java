fdf6fbbd425a1b396c4316f900567ff5d2ff8abd
piomin <piotr.minkowski@gmail.com>
2024-02-18 23:09:11 +0100

    secure bundles improvements

--- ssl/secure-caller-bundle/src/main/java/pl/piomin/services/caller/SecureCallerBundle.java
+++ ssl/secure-caller-bundle/src/main/java/pl/piomin/services/caller/SecureCallerBundle.java
@@ -1,22 +1,43 @@
 package pl.piomin.services.caller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.security.KeyStoreException;
 
 
 @SpringBootApplication
 public class SecureCallerBundle {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SecureCallerBundle.class);
     public static void main(String[] args) {
         SpringApplication.run(SecureCallerBundle.class, args);
     }
 
-    @Bean
+    @Autowired
+    ApplicationContext context;
+
+    @Bean("restTemplate")
     RestTemplate builder(RestTemplateBuilder builder, SslBundles sslBundles) {
+        sslBundles.addBundleUpdateHandler("client", sslBundle -> {
+            try {
+                LOG.info("Bundle updated: " + sslBundle.getStores().getKeyStore().getCertificate("certificate"));
+            } catch (KeyStoreException e) {
+                LOG.error("Error on getting certificate", e);
+            }
+            DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) context.getAutowireCapableBeanFactory();
+            registry.destroySingleton("restTemplate");
+            registry.registerSingleton("restTemplate", builder.setSslBundle(sslBundle).build());
+        });
         return builder.setSslBundle(sslBundles.getBundle("client")).build();
     }
 }
