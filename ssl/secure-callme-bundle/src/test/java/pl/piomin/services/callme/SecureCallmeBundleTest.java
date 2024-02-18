6253fdc5c462d9d61d2717366d415b2c64518b5d
piomin <piotr.minkowski@gmail.com>
2024-02-19 00:36:59 +0100

    fix tests 1

--- ssl/secure-callme-bundle/src/test/java/pl/piomin/services/callme/SecureCallmeBundleTest.java
+++ ssl/secure-callme-bundle/src/test/java/pl/piomin/services/callme/SecureCallmeBundleTest.java
@@ -2,12 +2,23 @@ package pl.piomin.services.callme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import javax.net.ssl.SSLContext;
+
+import java.net.http.HttpClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SecureCallmeBundleTest {
 
     @Autowired
@@ -17,5 +28,15 @@ public class SecureCallmeBundleTest {
     void call() {
         String res = restTemplate.getForObject("/callme", String.class);
         assertEquals("I'm secure-callme-bundle!", res);
+    }
+
+    @TestConfiguration
+    public static class TestRestTemplateConfiguration {
+        @Bean
+        @Primary
+        TestRestTemplate testRestTemplate(RestTemplateBuilder builder, SslBundles sslBundles) {
+            builder.setSslBundle(sslBundles.getBundle("server"));
+            return new TestRestTemplate(builder);
+        }
     }
 }
