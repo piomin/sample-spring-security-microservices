3123f20c033bcebe85fda9774d5605c5c53ae61d
piomin <piotr.minkowski@gmail.com>
2024-02-22 17:46:42 +0100

    add RestClient with SslBundle

--- ssl/secure-caller-bundle/src/main/java/pl/piomin/services/caller/SecureCallerBundle.java
+++ ssl/secure-caller-bundle/src/main/java/pl/piomin/services/caller/SecureCallerBundle.java
@@ -3,13 +3,16 @@ package pl.piomin.services.caller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.client.RestClientSsl;
 import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.security.KeyStoreException;
@@ -39,5 +42,15 @@ public class SecureCallerBundle {
             registry.registerSingleton("restTemplate", builder.setSslBundle(sslBundle).build());
         });
         return builder.setSslBundle(sslBundles.getBundle("client")).build();
+    }
+
+    @Value("${client.url}")
+    String clientUrl;
+
+    @Bean
+    RestClient restClient(RestClient.Builder restClientBuilder, RestClientSsl ssl) {
+        return restClientBuilder.baseUrl(clientUrl)
+                .apply(ssl.fromBundle("client"))
+                .build();
     }
 }
--- ssl/secure-caller-bundle/src/main/java/pl/piomin/services/caller/controller/SecureCallerBundleController.java
+++ ssl/secure-caller-bundle/src/main/java/pl/piomin/services/caller/controller/SecureCallerBundleController.java
@@ -3,23 +3,32 @@ package pl.piomin.services.caller.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SecureCallerBundleController {
 
     RestTemplate restTemplate;
+    RestClient restClient;
 
     @Value("${client.url}")
     String clientUrl;
 
-    public SecureCallerBundleController(RestTemplate restTemplate) {
+    public SecureCallerBundleController(RestTemplate restTemplate, RestClient restClient) {
         this.restTemplate = restTemplate;
+        this.restClient = restClient;
     }
 
     @GetMapping("/caller")
     public String call() {
         return "I'm `secure-caller`! calling... " +
                 restTemplate.getForObject(clientUrl, String.class);
+    }
+
+    @GetMapping("/caller/client")
+    public String call2() {
+        return "I'm `secure-caller`! calling... " +
+                restClient.get().retrieve().body(String.class);
     }
 }
