a18feedf24e1df6ad9d87b11543f40284697ab6e
piomin <piotr.minkowski@gmail.com>
2024-02-25 23:54:44 +0100

    update oauth2 to spring boot 3 + restructure

--- oauth/callme/src/main/java/pl/piomin/samples/security/callme/config/SecurityConfig.java
+++ oauth/callme/src/main/java/pl/piomin/samples/security/callme/config/SecurityConfig.java
@@ -1,19 +1,18 @@
 package pl.piomin.samples.security.callme.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
-				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+				.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 	}
 }
