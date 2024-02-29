81ddc2a4be8286297a45485c6da9228a1aaa2c12
piomin <piotr.minkowski@gmail.com>
2024-02-29 23:25:12 +0100

    improvements (2)

--- oauth/caller/src/main/java/pl/piomin/samples/security/caller/controller/CallerController.java
+++ oauth/caller/src/main/java/pl/piomin/samples/security/caller/controller/CallerController.java
@@ -4,24 +4,18 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/caller")
 public class CallerController {
 
-    private RestTemplate restTemplate;
     private WebClient webClient;
 
-    public CallerController(RestTemplate restTemplate, WebClient webClient) {
-        this.restTemplate = restTemplate;
+    public CallerController(WebClient webClient) {
         this.webClient = webClient;
     }
 
--- oauth/gateway/src/main/java/pl/piomin/samples/security/gateway/config/SecurityConfig.java
+++ oauth/gateway/src/main/java/pl/piomin/samples/security/gateway/config/SecurityConfig.java
@@ -1,22 +1,11 @@
 package pl.piomin.samples.security.gateway.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
