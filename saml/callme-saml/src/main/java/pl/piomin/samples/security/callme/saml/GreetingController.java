ddb776087ca05e4f8a886907f56e2c8f03ec935d
piomin <piotr.minkowski@gmail.com>
2024-10-28 10:24:41 +0100

    add saml (2)

---
+++ saml/callme-saml/src/main/java/pl/piomin/samples/security/callme/saml/GreetingController.java
@@ -0,0 +1,24 @@
+package pl.piomin.samples.security.callme.saml;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String greeting() {
+        return "I'm SAML user!";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String admin() {
+        return "I'm SAML admin!";
+    }
+
+}
--- saml/callme-saml/src/main/java/pl/piomin/samples/security/callme/saml/SecurityConfig.java
+++ saml/callme-saml/src/main/java/pl/piomin/samples/security/callme/saml/SecurityConfig.java
@@ -1,10 +1,19 @@
 package pl.piomin.samples.security.callme.saml;
 
+import org.opensaml.core.xml.schema.impl.XSStringImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.saml2.core.Saml2ResponseValidatorResult;
+import org.springframework.security.saml2.provider.service.authentication.OpenSaml4AuthenticationProvider;
+import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
 import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
 import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
@@ -16,28 +25,45 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
-//    private final RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        DefaultRelyingPartyRegistrationResolver relyingPartyRegistrationResolver = new DefaultRelyingPartyRegistrationResolver(
-//                this.relyingPartyRegistrationRepository);
-//        Saml2MetadataFilter filter = new Saml2MetadataFilter(relyingPartyRegistrationResolver, new OpenSamlMetadataResolver());
-//
+
+        OpenSaml4AuthenticationProvider provider = new OpenSaml4AuthenticationProvider();
+        provider.setResponseValidator((responseToken) -> {
+            Saml2ResponseValidatorResult result = OpenSaml4AuthenticationProvider
+                    .createDefaultResponseValidator()
+                    .convert(responseToken);
+            if (!result.getErrors().isEmpty()) {
+                String inResponseTo = responseToken.getResponse().getInResponseTo();
+//                throw new CustomSaml2AuthenticationException(result, inResponseTo);
+            }
+            LOG.info("!!! SAML2 RESPONSE: " + responseToken.getToken().getSaml2Response());
+            return result;
+        });
+
+        provider.setResponseAuthenticationConverter(token -> {
+            var auth = OpenSaml4AuthenticationProvider.createDefaultResponseAuthenticationConverter().convert(token);
+            LOG.info("AUTHORITIES: " + auth.getAuthorities());
+            token.getResponse().getAssertions().forEach(it -> it.getAttributeStatements()
+                    .forEach(st -> st.getAttributes().forEach(attr -> LOG.info(attr.getName() + "=" + ((XSStringImpl) attr.getAttributeValues().get(0)).getValue()))));
+            return auth;
+        });
+
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize.anyRequest()
                         .authenticated())
-                .saml2Login(withDefaults())
+                .saml2Login(saml2 -> saml2
+                        .authenticationManager(new ProviderManager(provider))
+                )
                 .saml2Metadata(withDefaults())
 //                .saml2Logout(withDefaults())
-//                .addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class)
                 ;
         return http.build();
     }
 
-//    public SecurityConfig(RelyingPartyRegistrationRepository relyingPartyRegistrationRepository) {
-//        this.relyingPartyRegistrationRepository = relyingPartyRegistrationRepository;
-//    }
 }
