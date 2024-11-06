da8b07d22e637df275dfd4e891c531a43e937d72
piomin <piotr.minkowski@gmail.com>
2024-11-06 08:20:49 +0100

    test

--- oauth/callme/src/main/java/pl/piomin/samples/security/callme/config/SecurityConfig.java
+++ oauth/callme/src/main/java/pl/piomin/samples/security/callme/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -16,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2Login(withDefaults())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
--- oauth/callme/src/main/java/pl/piomin/samples/security/callme/controller/CallmeController.java
+++ oauth/callme/src/main/java/pl/piomin/samples/security/callme/controller/CallmeController.java
@@ -4,6 +4,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/callme")
 public class CallmeController {
 
-    @PreAuthorize("hasAuthority('SCOPE_TEST')")
+    @PreAuthorize("hasAuthority('SCOPE_amr')")
     @GetMapping("/ping")
     public String ping() {
         SecurityContext context = SecurityContextHolder.getContext();
         Authentication authentication = context.getAuthentication();
         return "Scopes: " + authentication.getAuthorities();
+    }
+
+    @GetMapping(value = "/token")
+    public String getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+        return authorizedClient.getAccessToken().getTokenValue();
     }
 }
